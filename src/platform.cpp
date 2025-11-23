187bc88254b41a2bbd5d327645d65717847c9b02
shinrax2 <shinra.ubu1@yahoo.de>
2025-11-23 15:01:37 +0100

    Update platform.cpp

--- src/platform.cpp
+++ src/platform.cpp
@@ -68,22 +68,22 @@ void blt::platform::InitPlatform()
 		{
 			if (std::filesystem::exists("IPHLPAPI.dll") and !std::filesystem::exists("WSOCK32.dll"))
 			{
-				if (MoveFileEx("IPHLPAPI.dll", "IPHLPAPI.dll.old", MOVEFILE_REPLACE_EXISTING or MOVEFILE_WRITE_THROUGH) == 0)
+				if (MoveFileEx("IPHLPAPI.dll", "IPHLPAPI.dll.old") == 0)
 				{
 					MessageBox(0, std::format("{}", GetLastError()).c_str(), "SBLT DLL Downloader", MB_OK);
 				}
-				if (MoveFileEx("updater/IPHLPAPI.dll", "IPHLPAPI.dll", MOVEFILE_REPLACE_EXISTING or MOVEFILE_WRITE_THROUGH) == 0)
+				if (MoveFileEx("updater/IPHLPAPI.dll", "IPHLPAPI.dll") == 0)
 				{
 					MessageBox(0, std::format("{}", GetLastError()).c_str(), "SBLT DLL Downloader", MB_OK);
 				}
 			}
 			if (std::filesystem::exists("WSOCK32.dll") and !std::filesystem::exists("IPHLPAPI.dll"))
 			{
-				if (MoveFileEx("WSOCK32.dll", "WSOCK32.dll.old", MOVEFILE_REPLACE_EXISTING or MOVEFILE_WRITE_THROUGH) == 0)
+				if (MoveFileEx("WSOCK32.dll", "WSOCK32.dll.old") == 0)
 				{
 					MessageBox(0, std::format("{}", GetLastError()).c_str(), "SBLT DLL Downloader", MB_OK);
 				}
-				if (MoveFileEx("updater/WSOCK32.dll", "WSOCK32.dll", MOVEFILE_REPLACE_EXISTING or MOVEFILE_WRITE_THROUGH) == 0)
+				if (MoveFileEx("updater/WSOCK32.dll", "WSOCK32.dll") == 0)
 				{
 					MessageBox(0, std::format("{}", GetLastError()).c_str(), "SBLT DLL Downloader", MB_OK);
 				}
